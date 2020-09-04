534061e37f1b80bffe07fad18db6d21ab5709910
Easton Gorishek <ieaston26@gmail.com>
2020-09-04 12:24:36 -0600

    Added first test and refactored to include beforeEach fnc

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default props =>
   <form>
     <input
+      autoFocus
       type='text'
       className="new-todo"
+      value={props.currentTodo}
+      onChange={props.handleNewTodoChange}
       placeholder="What needs to be done?"/>
   </form>
